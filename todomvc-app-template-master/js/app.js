e2027ba60839028cc17d59a21a2935d5c9d1a4fa
jerrysheen <jerryshin961115@gmail.com>
2019-08-29 17:15:12 +0800

    new todo input function

--- todomvc-app-template-master/js/app.js
+++ todomvc-app-template-master/js/app.js
@@ -1,6 +1,34 @@
-(function (window) {
-	'use strict';
+(function (Vue) {
+	let localData = [
+		{
+			id:"1",
+			content:"Vue.js",
+			completed:false,//un do
+		},
+		{
+			id:"2",
+			content:"11111Vue.js",
+			completed:true,//un do
+		},
+	]
 
-	// Your starting point. Enjoy the ride!
+	new Vue({
+		el:"#todoapp",
+		data:{
+			title:"todos",
+			newTodo:"",
+			items:[...localData]
 
-})(window);
+		},
+		methods: {
+			deleteItem:function(id,event){
+				console.log(typeof(id))
+				this.items = this.items.filter(item=>{return item.id!=id})
+				console.log(this.items)
+			},
+			addTodo:function(){
+				console.log(this.newTodo)
+			}	
+		},
+	})
+})(Vue);
