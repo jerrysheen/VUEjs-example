cdc4b2679d0024406d57ae6c250b5ea68a3489b1
unknown <jerryshin961115@gmail.com>
2019-08-30 00:01:12 +0800

    toggles waiting to be done

--- todomvc-app-template-master/js/app.js
+++ todomvc-app-template-master/js/app.js
@@ -16,18 +16,40 @@
 		el:"#todoapp",
 		data:{
 			title:"todos",
-			newTodo:"",
 			items:[...localData]
-
+		},
+		computed:{
+			needToDo:function(){
+				return this.items.filter(item =>{return item.completed === false}).length
+			}
 		},
 		methods: {
 			deleteItem:function(id,event){
-				console.log(typeof(id))
+				console.log("item id",typeof(id))
 				this.items = this.items.filter(item=>{return item.id!=id})
 				console.log(this.items)
 			},
-			addTodo:function(){
-				console.log(this.newTodo)
+			addTodo:function(event){
+				console.log(event.target.value)
+				const newTodo = event.target.value.trim()
+				const isSame = this.items.filter(item =>{return item.content === newTodo}).length
+				const length = this.items.length
+				console.log(isSame)
+				if(isSame === 0 && newTodo!=""){
+					// not same
+					this.items = [...this.items,{
+						id:this.items[length-1].id+1,
+						content:newTodo,
+						completed:false
+					}]
+					event.target.value = ""
+					console.log("length",length)
+					console.log(this.items)
+				}
+				else{
+					alert("you already have this todo item or your enter is empty")
+				}
+				
 			}	
 		},
 	})
