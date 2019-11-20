e65996855566a42b6bb0cde90a558bc17af336cc
unknown <jerryshin961115@gmail.com>
2019-11-20 16:25:01 +0800

    search bar reset function, add new member function(without input verification)

--- MemberManagementSystem/member-management-system/src/api/member.js
+++ MemberManagementSystem/member-management-system/src/api/member.js
@@ -6,5 +6,14 @@ export default {
             url: '/member/list',
             method: 'get'
         })
+    },
+
+    //search a cartain data, and get page,size information
+    search(page, size, searchMap){
+        return request({
+            url: `/member/list/search/${page}/${size}`,
+            method: 'post',
+            data: searchMap
+        })
     }
 }
--- MemberManagementSystem/member-management-system/src/views/member/index.vue
+++ MemberManagementSystem/member-management-system/src/views/member/index.vue
@@ -1,68 +1,169 @@
 <template>
   <div>
-    <el-table :data="list" height="350" border style="width: 100%">
-      <el-table-column prop="cardNum" label="ID" width="180"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-      <el-table-column label="支付类型" width="100">
-        <template slot-scope="scope">
-          <span style="margin-left: 10px">{{scope.row.payType|payTypeTrans}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="address" label="地址"> </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-            >编辑</el-button
-          >
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-            >删除</el-button
-          >
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
-</template>
+      <!--  search bar-->
+    <el-form :inline="true" :model="formInline" ref="formInline" class="demo-form-inline" 
+      style="margin-top:20px; margin-left:10px;">
+      <!--需要指定字段名prop才会可以重置 -->
+      <el-form-item prop= "id">
+        <el-input v-model="formInline.id" placeholder="ID"></el-input>
+      </el-form-item>
+      <el-form-item prop= "name">
+        <el-input v-model="formInline.name" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item prop= "payType">
+        <el-select v-model="formInline.payType" placeholder="支付类型">
+          <el-option label="现金" value="cash"></el-option>
+          <el-option label="微信" value="wechat"></el-option>
+          <el-option label="支付宝" value="alipay"></el-option>
+          <el-option label="银行卡" value="card"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="searchSubmit">查询</el-button>
+        <el-button type="primary" @click="serachReset('formInline')"  >重置</el-button>
+        <el-button type="primary" @click="dialogFormVisible = true">新增</el-button>
+      </el-form-item>
+    </el-form>
+    <!--  main table-->
+          <el-table :data="list" height="350" border style="width: 100%">
+            <el-table-column prop="cardNum" label="ID" width="180"> </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+            <el-table-column label="支付类型" width="100">
+              <template slot-scope="scope">
+                <span style="margin-left: 10px">{{
+                  scope.row.payType | payTypeTrans
+                }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="address" label="地址"> </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+                  >编辑</el-button
+                >
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click="handleDelete(scope.$index, scope.row)"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="block">
+            <el-pagination
+              @current-change="handleCurrentChange"
+              :current-page.sync="currentPage"
+              :page-size="pageSize"
+              layout="total, prev, pager, next"
+              :total="total"
+            >
+            </el-pagination>
+          </div>
 
-<script>
-import memberApi from "@/api/member.js";
+          <!--bumping dialog-->
+          <!-- Form -->
+          <el-dialog title="新增会员" :visible.sync="dialogFormVisible">
+            <el-form :model="formInline" style="label-position:right;">
+              <el-form-item label="会员账号" :label-width="formLabelWidth">
+                <el-input v-model="formInline.id" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="会员姓名" :label-width="formLabelWidth">
+                <el-input v-model="formInline.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="支付方式" :label-width="formLabelWidth">
+                <el-select v-model="formInline.payType" placeholder="支付方式">
+                  <el-option label="现金" value="cash"></el-option>
+                  <el-option label="微信" value="wechat"></el-option>
+                  <el-option label="支付宝" value="alipay"></el-option>
+                  <el-option label="银行卡" value="card"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="submitNewMember">确 定</el-button>
+            </div>
+          </el-dialog>
 
-const typeList = ["现金", "微信", "支付宝", "银行卡"];
+        </div>
+      </template>
 
-export default {
-  created() {
-    //初始化获取列表数据
-    this.fetchData();
-  },
-  data() {
-    return {
-      list: []
-    };
-  },
-  methods: {
-    fetchData() {
-      memberApi.getList().then(response => {
-        const resp = response.data.data;
-        console.log(resp);
-        this.list = resp;
-      });
-    },
-    handleEdit(index, row) {
-      console.log(index, row);
-    },
-    handleDelete(index, row) {
-      this.list.splice(index, 1);
-      console.log(index, row);
-    }
-  },
-  filters: {
-    payTypeTrans: function(value) {
-      return typeList[parseInt(value)-1];
-    }
-  }
-};
-</script>
+      <script>
+      import memberApi from "@/api/member.js";
 
-<style scoped></style>
+      const typeList = ["现金", "微信", "支付宝", "银行卡"];
+
+      export default {
+        created() {
+          //初始化获取列表数据
+          this.fetchData();
+        },
+        data() {
+          return {
+            formInline: {
+              id:'',
+              name: '',
+              payType: ''
+            },
+            list: [],
+            total: 0, // total data number
+            currentPage: 1,
+            pageSize: 10,
+            searchMap: {}, // search by certain requirement
+            // param for the dialog form used for add new member
+            dialogFormVisible: false,
+            formLabelWidth: '120px',
+          };
+        },
+        methods: {
+          fetchData() {
+            memberApi
+              .search(this.currentPage, this.pageSize, this.searchMap)
+              .then(response => {
+                const resp = response.data.data;
+                this.list = resp.rows
+                this.total = resp.total
+                console.log(resp);
+              });
+          },
+          // button method for search
+          searchSubmit() {
+            this.searchMap = this.formInline
+            this.fetchData()
+            console.log('submit!');
+            console.log('searchMap',this.formInline)
+          },
+          serachReset(formName) {
+            console.log("reset!")
+            this.$refs[formName].resetFields();
+          },
+          // button method for delete user data
+          handleEdit(index, row) {
+            console.log(index, row);
+          },
+          handleDelete(index, row) {
+            this.list.splice(index, 1);
+            console.log(index, row);
+          },
+          // pagination function
+          handleCurrentChange(val) {
+            this.pagination = this.val
+            this.fetchData()
+          },
+          // dialog button for add new member
+          submitNewMember(){
+            console.log("submit!",this.formInline)
+            this.dialogFormVisible = false
+          },
+        },
+        filters: {
+          payTypeTrans: function(value) {
+            return typeList[parseInt(value) - 1];
+          }
+        }
+      };
+    </script>
+
+    <style scoped></style></div
+></template>
